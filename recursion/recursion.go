1c49fc4aeccf8bb85bab944fe4ca5442f608132d
Abdul-Azeez Abdul-Azeem <abdulazeemabdulazeez@gmail.com>
2026-08-03 00:47:58 +0100

    feat: work with qsort.

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -10,3 +10,82 @@ func Countdown(num int) {
 	num--
 	Countdown(num)
 }
+
+func RecursiveSum(list []int) int {
+	if len(list) == 0 {
+		return 0
+	}
+
+	return list[0] + RecursiveSum(list[1:])
+}
+
+func RecursiveCount(list []int) int {
+	if len(list) == 0 {
+		return 0
+	}
+
+	return 1 + RecursiveCount(list[1:])
+}
+
+func RecursiveFindMax(list []int) int {
+	if len(list) == 0 {
+		return 0
+	}
+
+	if len(list) == 1 {
+		return list[0]
+	}
+
+	maxOfRest := RecursiveFindMax(list[1:])
+
+	if list[0] > maxOfRest {
+		return list[0]
+	}
+
+	return maxOfRest
+}
+
+func RecursiveBinarySearch(list []int, low int, high int, search int) int {
+
+	if low > high {
+		return -1
+	}
+
+	mid := low + (high-low)/2
+
+	if list[mid] == search {
+		return mid
+	}
+
+	if list[mid] > search {
+		return RecursiveBinarySearch(list, low, mid-1, search)
+	}
+
+	return RecursiveBinarySearch(list, mid+1, high, search)
+}
+
+func QuickSort(list []int, low, high int) {
+	if low < high {
+		pivotIndex := partition(list, low, high)
+		QuickSort(list, low, pivotIndex-1)
+		QuickSort(list, pivotIndex+1, high)
+	}
+}
+
+func partition(list []int, low, high int) int {
+	pivot := list[high]
+
+	i := low - 1
+	var j int
+
+	for j = low; j < high; j++ {
+		if pivot >= list[j] {
+			i++
+			list[j], list[i] = list[i], list[j]
+		}
+	}
+
+	list[i+1], list[j] = list[j], list[i+1]
+
+	return i + 1
+}
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -1,8 +1,27 @@
 package recursion
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCountDown(t *testing.T) {
 	time := 3
 	Countdown(time)
+}
+
+func TestQuickSort(t *testing.T) {
+	list := []int{2, 6, 8, 5, 4}
+
+	expectedFirstItem := 2
+	expectedLastItem := 8
+
+	QuickSort(list, 0, len(list)-1)
+
+	if list[0] != expectedFirstItem {
+		t.Errorf("Expected first item to be %d, found %d", expectedFirstItem, list[0])
+	}
+
+	if list[len(list)-1] != expectedLastItem {
+		t.Errorf("Expected last item to be %d, found %d", expectedLastItem, list[len(list)-1])
+	}
 }
